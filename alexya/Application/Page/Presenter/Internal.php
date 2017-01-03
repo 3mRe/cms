90fbe973079c4ae3ccfe88c357ca6f3b0492642d
Manulaiko <manulaiko@gmail.com>
2017-01-03 11:49:37 +0100

    Created presenters

---
+++ alexya/Application/Page/Presenter/Internal.php
@@ -0,0 +1,27 @@
+<?php
+namespace Application\Page\Presenter;
+
+use Alexya\Foundation\Presenter;
+
+/**
+ * Internal page presenter.
+ *
+ * @author Manulaiko <manulaiko@gmail.com>
+ */
+class Internal extends Presenter
+{
+    /**
+     * Renders and returns the page.
+     *
+     * @return string Page content.
+     */
+    public function render() : string
+    {
+        $this->_triad->View->set("name", ($this->_request->uri()[2] ?? ""));
+
+        // Call the render method here so it redirects now instead of when the view is being rendered.
+        $this->_triad->View->set("module", $this->_triad->children->module->Controller->render());
+
+        // TODO $this->_triad->View->set("news", Model::latest(10, "id", "news"));
+    }
+}
